97de4051c1f3e65f6ede14de91911e9709d655da
juancarlos.pmera <juancarlos.pmera@XP520021.DSIC.ES>
2023-09-20 11:38:29 +0200

    Inicial. Creada estructura de carpetas. copiadas properties del proyecto vivienda. Actualizado el puerto a 9091

---
+++ src/main/java/com/melilla/REST_vivienda/RestViviendaApplication.java
@@ -0,0 +1,13 @@
+package com.melilla.REST_vivienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestViviendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestViviendaApplication.class, args);
+	}
+
+}
